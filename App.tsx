d5ffd88d5a043375d286a8d8fdda5352f30ad1d5
lucianobs1 <lucianojob1@hotmail.com>
2023-01-18 11:24:12 -0300

    added utils and updated configs in app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -8,6 +7,7 @@ import {
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,26 +22,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Olá</Text>
+    <>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
